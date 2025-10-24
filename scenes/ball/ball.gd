fa9b2e00fd077c017ec41aa5239d5d31e0c86fca
clefalto <hamomelett@gmail.com>
2025-10-23 23:24:09 -0700

    board is HORIZONTAL!

---
+++ scenes/ball/ball.gd
@@ -0,0 +1,4 @@
+extends RigidBody3D
+
+func _ready():
+	add_constant_force(Vector3(10.0, 0, 0))
--- scenes/test/three_dimensions.gd
+++ scenes/test/three_dimensions.gd
@@ -1,1 +1,15 @@
 extends Node3D
+
+@export var ball_prefab: PackedScene
+@export var ball_spawn_point: Node3D
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("DEBUG_spawn_ball"):
+		
+		spawn_ball()
+
+func spawn_ball():
+	print("hello")
+	var ball = ball_prefab.instantiate()
+	ball.position = ball_spawn_point.position
+	add_child(ball)
