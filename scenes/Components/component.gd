4e2768ae4bff11778a5defd7f9eb6550e8fa41b2
clefalto <hamomelett@gmail.com>
2025-10-26 02:03:11 -0700

    increase size of window and move some stuff around

--- scenes/Components/component.gd
+++ scenes/Components/component.gd
@@ -1,3 +1,10 @@
 class_name Component extends Node
 
 @onready var score_manager = get_tree().get_first_node_in_group("ScoreManager")
+
+## does nothing, but lets you polymorphically (idk if that's a word) call trigger on any component. will be useful
+## when force-triggering components with other components and effects and things
+## should be overridden by inheritors with the actual behavior!!!!!!!!!
+func trigger():
+	print("triggered %s" % self)
+	pass
